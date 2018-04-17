6baff47e3a48d8052b3817a2d36e0cc9af932c08
jmuratov98 <38193373+jmuratov98@users.noreply.github.com>
2018-04-17 18:20:01 +0000

    Finished

--- p5/towers.cpp
+++ p5/towers.cpp
@@ -21,6 +21,9 @@ using std::endl;
 #include <getopt.h> // to parse long arguments.
 #include <cstdlib> // for atoi function
 
+void Hanoi(short n, short start, short end, short aux_rod);
+short findAux_rod(short start, short end);
+
 /* Here's a skeleton main function for processing the arguments. */
 int main(int argc, char *argv[]) {
 	// define long options
@@ -54,6 +57,57 @@ int main(int argc, char *argv[]) {
 
 	/* TODO: now that you have the options and arguments,
 	 * solve the puzzle. */
+	
+	// This is for test puposes 
+	/*cout << "start = " << start << endl;
+	cout << "end = " << end << endl;
+	cout << "number of disks = " << n << endl;*/
+
+	if(end > 3)
+	{
+		cout << "Can't have more then 3 pegs" << endl;
+		return 0;
+	}
+
+	//find auxillary rod
+	short aux_rod = findAux_rod(start,end);
+
+	Hanoi(n,start,end, aux_rod);
 
 	return 0;
+}
+
+void Hanoi(short n, short start, short end, short aux_rod)
+{
+	aux_rod = findAux_rod(start,end);
+	//cout << "aux_rod = " << aux_rod << endl;
+	if(n == 1)
+	{
+		cout << start << '\t' << end << endl;
+		return;
+	}
+	Hanoi(n-1,start,aux_rod,end);
+	cout << start << '\t' << end << endl;
+	Hanoi(n-1,aux_rod,end,start);
+}
+
+short findAux_rod(short start, short end)
+{
+	short aux_rod;
+	if(start == 1)
+	{
+		if(end == 3) aux_rod = 2;
+		if(end == 2) aux_rod = 3;
+	}
+	else if(start == 2)
+	{
+		if(end == 1) aux_rod = 3;
+		if(end == 3) aux_rod = 1;
+	}
+	else if(start == 3)
+	{
+		if(end == 1) aux_rod = 2;
+		if(end == 2) aux_rod = 1;
+	}
+	return aux_rod;
 }
